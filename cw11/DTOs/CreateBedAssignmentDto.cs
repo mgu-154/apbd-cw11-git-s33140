99ffd9c17c2d022609d1289116cdf3f768492d0b
Maciek Guevara <maciej@guevara.pl>
2026-05-28 21:59:02 +0200

    Stworzenie dto potrzebnego do zwracania zapytan HTTP POST

---
+++ cw11/DTOs/CreateBedAssignmentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace cw11.DTOs;
+
+public class CreateBedAssignmentDto
+{
+    public DateTime From { get; set; }
+    public DateTime? To { get; set; }
+    public string BedType { get; set; }
+    public string Ward { get; set; }
+}
