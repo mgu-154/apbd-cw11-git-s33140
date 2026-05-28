b4daf5d7b5c23b6ea14709fce940512188db3bd2
Maciek Guevara <maciej@guevara.pl>
2026-05-28 20:11:09 +0200

    Dodanie klasy obslugujacej wyjatki

--- cw11/Exceptions/NotFoundException.cs
+++ cw11/Exceptions/NotFoundException.cs
@@ -1,6 +1,16 @@
 ﻿namespace cw11.Exceptions;
 
-public class NotFoundException
+public class NotFoundException : Exception
 {
-    
+    public NotFoundException()
+    {
+    }
+
+    public NotFoundException(string? message) : base(message)
+    {
+    }
+
+    public NotFoundException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
 }
